b746a5508e1de2cd0dfa4635fa342a98e65f85ce
shanomm <lolformon@gmail.com>
2025-11-23 14:59:40 +0700

    feat: tracking question number

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,13 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  List<bool> answers = [false, true, true];
+  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 25.0, color: Colors.white),
               ),
@@ -58,6 +65,9 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 setState(() {
                   scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+                  if (questionNumber < questions.length) {
+                    questionNumber++;
+                  }
                 });
               },
             ),
@@ -75,6 +85,9 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 setState(() {
                   scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+                  if (questionNumber + 1 < questions.length) {
+                    questionNumber++;
+                  }
                 });
               },
             ),
